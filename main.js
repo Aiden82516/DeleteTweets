f73693e7ffb93e06e20f1bdf45ea3bb9a7b38352
Aiden82516 <hxxsxax@gmail.com>
2025-02-08 21:51:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
+var client_tid = "
+kcunqaKaeBf5aG1dtBVAvYb3bpbnh9+gN/7b1i5ZqBxAr0uZ1rekgwvxxWmhA38F1tt8xpIH3Lgycd9oSvFCcvl3U0aDkg
+"; // replace by X-Client-Transaction-Id value
 var client_uuid = "***"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
@@ -8,7 +11,7 @@ var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "iamabobgirl" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
